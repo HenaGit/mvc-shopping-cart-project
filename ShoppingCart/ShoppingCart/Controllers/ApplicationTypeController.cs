74e6987a71c3983596ffb82e58c2c305486d0c6b
henokg <henokg@insa.com>
2023-03-17 15:14:10 -0600

    Add index and create for application type

---
+++ ShoppingCart/ShoppingCart/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Data;
+using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ApplicationTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> applicationTypeList = _db.ApplicationType;
+            return View(applicationTypeList);
+        }
+        //GET -CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //GET -POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ApplicationType applicationType)
+        {
+            _db.ApplicationType.Add(applicationType);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
