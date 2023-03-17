43a058dcb86e6cf475dafddf681342fc3ae03fc1
henokg <henokg@insa.com>
2023-03-17 13:51:46 -0600

    Add category index page

--- ShoppingCart/ShoppingCart/Controllers/CategoryController.cs
+++ ShoppingCart/ShoppingCart/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Data;
+using ShoppingCart.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,15 @@ namespace ShoppingCart.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> catList = _db.Category;
+            return View(catList);
         }
     }
 }
