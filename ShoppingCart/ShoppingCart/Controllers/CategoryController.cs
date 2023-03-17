f41b45f4fcd5f73736a651c83b2ad97799ba47e3
henokg <henokg@insa.com>
2023-03-17 16:35:48 -0600

    Add update and delete for category

--- ShoppingCart/ShoppingCart/Controllers/CategoryController.cs
+++ ShoppingCart/ShoppingCart/Controllers/CategoryController.cs
@@ -39,5 +39,68 @@ namespace ShoppingCart.Controllers
             return View(category);
             
         }
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Category.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+
+        }
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Category.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var category = _db.Category.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
     }
 }
