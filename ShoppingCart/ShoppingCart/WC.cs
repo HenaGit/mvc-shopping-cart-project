ea9cd0b8c27c1ff684f2f01f0d771620d2145c24
henokg <henokg@insa.com>
2023-03-18 14:55:45 -0600

    Modify create and update for product

---
+++ ShoppingCart/ShoppingCart/WC.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+    public static class WC
+    {
+        public const string ImagePath = @"\images\product\";
+    }
+}
