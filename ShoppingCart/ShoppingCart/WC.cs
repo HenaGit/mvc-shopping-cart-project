06a2db064c1ed30a40de602fefde24f5ab05748b
henokg <henokg@insa.com>
2023-03-20 12:57:31 -0600

    Add name and phone number to registration

--- ShoppingCart/ShoppingCart/WC.cs
+++ ShoppingCart/ShoppingCart/WC.cs
@@ -9,5 +9,7 @@ namespace ShoppingCart
     {
         public const string ImagePath = @"\images\product\";
         public const string SessionCart = "ShoppingCartSession";
+        public const string AdminRole = "Admin";
+        public const string CustomerRole = "Customer";
     }
 }
