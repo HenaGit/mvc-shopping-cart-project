aafa854a05d88a5ae0f581f2f90fa11b51c05af3
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-02-12 03:58:48 +0300

    Implement Inquiry Repository

---
+++ ShoppingCart/ShoppingCart_DataAccess/Repository/IRepository/IInquiryHeaderRepository.cs
@@ -0,0 +1,16 @@
+﻿using ShoppingCart_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart_DataAccess.Repository.IRepository
+{
+    public interface IInquiryHeaderRepository : IRepository<InquiryHeader>
+    {
+        void Update(InquiryHeader obj);
+
+
+    }
+}
